d7e058567db87b00fef9ba4a48e059b25ace1df9
Patrik Ragnarsson <patrik@starkast.net>
2009-11-30 22:28:31 +0100

    Added changes that 2fb17c99bfcce4eba9b21d3a75b1fe267f43db40 reverted.

--- src/ui/panels/profile/ProfilePanel.java
+++ src/ui/panels/profile/ProfilePanel.java
@@ -7,7 +7,6 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
-import javax.swing.SwingConstants;
 
 public class ProfilePanel extends JPanel {
 
@@ -23,7 +22,7 @@ public class ProfilePanel extends JPanel {
 
    private JFrame mainFrame;
 
-   public ProfilePanel() {
+   public ProfilePanel(JLabel loggedInUserLabel) {
       setLayout(new BorderLayout());
       mainFrame = (JFrame)getParent();
       JTabbedPane tabPane = new JTabbedPane();
@@ -36,13 +35,7 @@ public class ProfilePanel extends JPanel {
       tabPane.add(NUTRITION, createNutritionTab());
       tabPane.add(WISHLIST, createWishlistTab());
       
-      add(createUserInfoLabel(), BorderLayout.SOUTH);
-   }
-   
-   private JComponent createUserInfoLabel() {
-	   String user = "John Doe";
-	   JLabel label = new JLabel("Inloggad som " + user, SwingConstants.RIGHT);
-	   return label;
+      add(loggedInUserLabel, BorderLayout.SOUTH);
    }
 
    private JComponent createOverviewTab(){
