065e5aef14e7906e80abf6061e3faa6b083f166a
Patrik Ragnarsson <patrik@starkast.net>
2009-12-07 23:48:24 +0100

    CardLayout instead of JTabbedPane

--- src/ui/panels/profile/ProfilePanel.java
+++ src/ui/panels/profile/ProfilePanel.java
@@ -1,15 +1,19 @@
 package ui.panels.profile;
 
 import java.awt.BorderLayout;
+import java.awt.CardLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.JTabbedPane;
 
-public class ProfilePanel extends JPanel {
+public class ProfilePanel extends JPanel implements ActionListener {
 
 	private static final long serialVersionUID = -3037636351809756514L;
 
@@ -20,41 +24,56 @@ public class ProfilePanel extends JPanel {
 	private final static String NUTRITION = "Näringsvärden";
 	private final static String WISHLIST = "Önskelista";
 
+	@SuppressWarnings("unused")
 	private JFrame mainFrame;
+	private JPanel cards; //a panel that uses CardLayout
 
 	public ProfilePanel(JLabel loggedInUserLabel) {
-		setLayout(new BorderLayout());
 		mainFrame = (JFrame)getParent();
-		JTabbedPane tabPane = new JTabbedPane();
-		add(tabPane);
-		tabPane.add(OVERVIEW, createOverviewTab());
-		tabPane.add(DISLIKES, createDislikesTab());
-		tabPane.add(GROUPS, createGroupsTab());
-		tabPane.add(PREFERENCES, createPreferencesTab());
-		tabPane.add(NUTRITION, createNutritionTab());
-		tabPane.add(WISHLIST, createWishlistTab());
-
+		
+        //Put the buttons in a JPanel to get a nicer look
+        JPanel buttonsPane = new JPanel(new GridLayout(0,1));
+        String buttons[] = { OVERVIEW, DISLIKES, GROUPS, PREFERENCES, 
+        		NUTRITION, WISHLIST};
+        for(int i = 0; i < buttons.length; i++) {
+            JButton button = new JButton(buttons[i]);
+            button.addActionListener(this);
+            buttonsPane.add(button);
+        }
+        
+        //Create the "cards" and the panel that contains the "cards"
+        cards = new JPanel(new CardLayout());
+        cards.add(createOverviewTab(), OVERVIEW);
+        cards.add(createDislikesTab(), DISLIKES);
+        cards.add(createGroupsTab(), GROUPS);
+        cards.add(createPreferencesTab(), PREFERENCES);
+        cards.add(createNutritionTab(), NUTRITION);
+        cards.add(createWishlistTab(), WISHLIST);
+        
+        add(buttonsPane, BorderLayout.PAGE_START);
+        add(cards, BorderLayout.CENTER);
 		add(loggedInUserLabel, BorderLayout.SOUTH);
 	}
 
+	public void actionPerformed(ActionEvent e) {
+        CardLayout cl = (CardLayout)(cards.getLayout());
+        cl.show(cards, (String)e.getActionCommand());
+	}
+	
 	private JComponent createOverviewTab(){
-		JPanel panel = new OverviewPanel();
-		return panel;
+		return new OverviewPanel();
 	}
 
 	private JComponent createDislikesTab(){
-		JPanel panel = new DislikePanel();
-		return panel;
+		return new DislikePanel();
 	}
 
 	private JComponent createGroupsTab(){
-		JPanel panel = new GroupPanel();
-		return panel;
+		return new GroupPanel();
 	}
 
 	private JComponent createPreferencesTab(){
-		PreferencesPanel panel = new PreferencesPanel();
-		return panel;
+		return new PreferencesPanel();
 	}
 
 	private JComponent createNutritionTab(){
@@ -62,6 +81,6 @@ public class ProfilePanel extends JPanel {
 	}
 
 	private JComponent createWishlistTab(){
-                return new WishlistPanel();
+		return new WishlistPanel();
 	}
 }
