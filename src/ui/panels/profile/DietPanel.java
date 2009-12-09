e7d748812ccfa6d76b6d66997c20d9044e74a3a8
Ulf Magnusson <ulf@h-88-48.a163.priv.bahnhof.se>
2009-12-09 13:18:48 +0100

    Tidied up PreferencesPanel

--- src/ui/panels/profile/DietPanel.java
+++ src/ui/panels/profile/DietPanel.java
@@ -1,5 +1,8 @@
 package ui.panels.profile;
 
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
@@ -7,6 +10,7 @@ import java.awt.event.ActionListener;
 import java.util.Vector;
 
 import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
 import javax.swing.ButtonGroup;
 import javax.swing.JList;
 import javax.swing.JPanel;
@@ -20,56 +24,58 @@ import dataObjects.Subject;
 
 public class DietPanel extends JPanel implements ActionListener {
 
-	/**
-	 *
-	 */
-	private static final long serialVersionUID = -4204456733995810769L;
+    /**
+     *
+     */
+    private static final long serialVersionUID = -4204456733995810769L;
+    
+    private static final int PREFERRED_WIDTH = 300;
 
-	/* getIngredientCategoriesFromFile(): 
+    /* getIngredientCategoriesFromFile(): 
      * Nötkött, Bröd, Ost, Ägg & mejeri, Fisk, Frukt, Vilt, Nötter & frön, 
      * Inälvsmat, Pasta & nudlar, Fläsk, Fågel, Soja & baljfrukter, 
      * Ris & korn, Skaldjur, Grönsaker, Smaksättning
-     */	
-	
-	/** 
-	 * Veganer undviker därmed animaliska produkter såsom kött, fågel, fisk, 
+     */ 
+    
+    /** 
+     * Veganer undviker därmed animaliska produkter såsom kött, fågel, fisk, 
      * ägg, mjölkprodukter, skinn, ylle, dun och päls.
      * http://sv.wikipedia.org/wiki/Vegan - 20091205
      */
-	String[] veganCategories = {"Nötkött", "Bröd", "Ost", "Ägg & mejeri", "Fisk", 
-			"Vilt", "Inälvsmat", "Fläsk", "Fågel", "Skaldjur"};
+    String[] veganCategories = {"Nötkött", "Bröd", "Ost", "Ägg & mejeri", "Fisk", 
+            "Vilt", "Inälvsmat", "Fläsk", "Fågel", "Skaldjur"};
     String[] emptyList = {};
 
     private static String NO_DIET_NAME = "ingen";
-	private JList contentList;
-	private Vector<Diet> diets;
-	private Subject user;
-	private Diet oldDiet;
-	@SuppressWarnings("unused")
-	private Diet currentDiet; //could be used for nicer messages
+    private JList contentList;
+    private Vector<Diet> diets;
+    private Subject user;
+    private Diet oldDiet;
+    @SuppressWarnings("unused")
+    private Diet currentDiet; //could be used for nicer messages
     
-	public class Diet {
-		private String name;
-		private String[] refusedCategories;
-		public Diet(String name, String[] refusedCategories) {
-			this.name = name;
-			this.refusedCategories = refusedCategories;;
-		}
-		public String[] getRefusedCategories() {
-			return refusedCategories;
-		}
-		public String getName() {
-			return name;
-		}
-	}
+    public class Diet {
+        private String name;
+        private String[] refusedCategories;
+        public Diet(String name, String[] refusedCategories) {
+            this.name = name;
+            this.refusedCategories = refusedCategories;;
+        }
+        public String[] getRefusedCategories() {
+            return refusedCategories;
+        }
+        public String getName() {
+            return name;
+        }
+    }
 
     public DietPanel() {
-    	super(new GridLayout(1,2));
-    	this.user = Session.getInstance().getUser();
-    	this.oldDiet = null;
-    	
+        setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+        this.user = Session.getInstance().getUser();
+        this.oldDiet = null;
+        
         diets = new Vector<Diet>();
-        String[] nothing 	= {};
+        String[] nothing    = {};
         contentList = new JList(nothing);
         contentList.setBackground(this.getBackground());
         contentList.setSelectionBackground(contentList.getBackground());
@@ -86,84 +92,101 @@ public class DietPanel extends JPanel implements ActionListener {
 
     public JPanel createButtons() {
         JPanel radioPanel = new JPanel(new GridLayout(0, 1));
+        radioPanel.setLayout(new BoxLayout(radioPanel, BoxLayout.LINE_AXIS));
+
         ButtonGroup group = new ButtonGroup();
         for (Diet diet : diets) {
-        	String name =  diet.getName();
-        	JRadioButton b;
-        	if(name.equals(NO_DIET_NAME))
-        		b = new JRadioButton(name, true);
-        	else
-        		b = new JRadioButton(name);
-        	b.setActionCommand(name);
-        	b.addActionListener(this);
-        	group.add(b);
-        	radioPanel.add(b);
+            String name =  diet.getName();
+            JRadioButton b;
+            if(name.equals(NO_DIET_NAME))
+                b = new JRadioButton(name, true);
+            else
+                b = new JRadioButton(name);
+            b.setActionCommand(name);
+            b.addActionListener(this);
+            group.add(b);
+            radioPanel.add(b);
         }
-		TitledBorder tb = BorderFactory.createTitledBorder(
-				BorderFactory.createEtchedBorder(),
-				"Möjlig specialkost",
-				TitledBorder.CENTER,
-				TitledBorder.CENTER,
-				new Font("Arial", Font.BOLD, 13)
-		);
-		radioPanel.setBorder(tb);
-		return radioPanel;
+
+        TitledBorder tb = BorderFactory.createTitledBorder(
+                BorderFactory.createEtchedBorder(),
+                "Specialkoster",
+                TitledBorder.LEFT,
+                TitledBorder.TOP
+                //new Font("Arial", Font.BOLD, 13)
+        );
+
+        radioPanel.setBorder(tb);
+
+        radioPanel.setAlignmentX(Component.LEFT_ALIGNMENT);
+        return radioPanel;
     }
 
     public JPanel showList(JList list) {
-    	JPanel panel = new JPanel();
-    	panel.add(list);
-		TitledBorder tb = BorderFactory.createTitledBorder(
-				BorderFactory.createEtchedBorder(),
-				"Kategorier som exkluderas",
-				TitledBorder.CENTER,
-				TitledBorder.CENTER,
-				new Font("Arial", Font.BOLD, 13)
-		);
-		panel.setBorder(tb);
-		return panel;
+        JPanel panel = new JPanel();
+        panel.setLayout(new BorderLayout());
+
+        panel.add(list, BorderLayout.CENTER);
+        TitledBorder tb = BorderFactory.createTitledBorder(
+                BorderFactory.createEtchedBorder(),
+                "Kategorier som utesluts",
+                TitledBorder.LEFT,
+                TitledBorder.TOP,
+                new Font("Arial", Font.PLAIN, 13)
+        );
+        panel.setBorder(tb);
+
+        panel.setAlignmentX(Component.LEFT_ALIGNMENT);
+        return panel;
     }
     
     public void exclude(Diet diet) {
-    	currentDiet = diet;
-    	// reverse the last choose before adding new stuff
-    	if(oldDiet != null) {
-        	String[] oldRefusedCategories = oldDiet.getRefusedCategories();
-        	for(int i = 0; i < oldRefusedCategories.length; i++) {
-        		System.out.println("remove: " + oldRefusedCategories[i]);
-        		user.removeRefusedCategory(oldRefusedCategories[i]);
-        	}    		
-    	}
-    	String[] refusedCategories = diet.getRefusedCategories();
-    	for(int i = 0; i < refusedCategories.length; i++) {
-    		System.out.println("add: " + refusedCategories[i]);
-    		user.addRefusedCategory(refusedCategories[i]);	
-    	}
-    	oldDiet = diet;
+        currentDiet = diet;
+        // reverse the last choose before adding new stuff
+        if(oldDiet != null) {
+            String[] oldRefusedCategories = oldDiet.getRefusedCategories();
+            for(int i = 0; i < oldRefusedCategories.length; i++) {
+                System.out.println("remove: " + oldRefusedCategories[i]);
+                user.removeRefusedCategory(oldRefusedCategories[i]);
+            }           
+        }
+        String[] refusedCategories = diet.getRefusedCategories();
+        for(int i = 0; i < refusedCategories.length; i++) {
+            System.out.println("add: " + refusedCategories[i]);
+            user.addRefusedCategory(refusedCategories[i]);  
+        }
+        oldDiet = diet;
     }
-	
+    
     public void flashMsg(String msg) {
-		StatusPanel.getInstance().flash(msg, StatusPanel.INFO);
-	}
+        StatusPanel.getInstance().flash(msg, StatusPanel.INFO);
+    }
 
-	public void flashErrorMsg(String msg) {
-		StatusPanel.getInstance().flash(msg, StatusPanel.ERROR);
-	}	
-	
+    public void flashErrorMsg(String msg) {
+        StatusPanel.getInstance().flash(msg, StatusPanel.ERROR);
+    }   
+    
     // View the LifeStyle list depending on which one is selected for the moment
     public void actionPerformed(ActionEvent e) {
-    	for (Diet diet : diets) {
-    		String name = diet.getName();
-    		if(name.equals(e.getActionCommand())) {
-    			// show which categories are excluded
-    			contentList.setListData(diet.getRefusedCategories());
-    			// exclude categories
-    			exclude(diet);
-    			if(name.equals(NO_DIET_NAME))
-    				flashMsg("Du valde att inte ha någon specialkost");
-    			else
-    				flashMsg("Du har valt " + name + " som specialkost");
-    		}
-		}
+        for (Diet diet : diets) {
+            String name = diet.getName();
+            if(name.equals(e.getActionCommand())) {
+                // show which categories are excluded
+                contentList.setListData(diet.getRefusedCategories());
+                // exclude categories
+                exclude(diet);
+                if(name.equals(NO_DIET_NAME))
+                    flashMsg("Du valde att inte ha någon specialkost");
+                else
+                    flashMsg("Du har valt " + name + " som specialkost");
+            }
+        }
+    }
+
+    @Override
+    public Dimension getPreferredSize() {
+        Dimension size = super.getPreferredSize();
+        size.width = PREFERRED_WIDTH;
+        return size;
     }
 }
--- src/ui/panels/profile/PreferencesPanel.java
+++ src/ui/panels/profile/PreferencesPanel.java
@@ -1,6 +1,7 @@
 package ui.panels.profile;
 
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
@@ -11,6 +12,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.BorderFactory;
+import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.ButtonGroup;
 import javax.swing.JLabel;
@@ -31,31 +33,42 @@ import dataObjects.Subject;
 
 public class PreferencesPanel extends JPanel implements ActionListener {
 
-	private static final long serialVersionUID = -9011866376505097065L;
-	private Subject user;
-	private PlanVariables pv;
+    private static final long serialVersionUID = -9011866376505097065L;
 
-	public PreferencesPanel() {
-		this.user 	= Session.getInstance().getUser();
-		this.pv 	= new PlanVariables();
-		
-        setPreferredSize(new Dimension(800, 600));
+    private static final int RADIO_PANEL_SPACING = 10;
+
+    private static final int DIET_PANEL_PREFERRED_WIDTH = 300;
+
+    private Subject user;
+    private PlanVariables pv;
+
+    public PreferencesPanel() {
+        this.user   = Session.getInstance().getUser();
+        this.pv     = new PlanVariables();
+
         setLayout(new GridBagLayout());
+
         GridBagConstraints c = new GridBagConstraints();
-        c.fill = GridBagConstraints.BOTH;
-        c.weightx = 0.5;
-        c.weighty = 0.5;
         c.gridx = 0;
         c.gridy = 0;
-        c.insets = new Insets(5, 5, 5, 5);
+        c.fill = GridBagConstraints.BOTH;
+        c.weightx = 0.5;
+        c.weighty = 1.0;
+        //c.insets = new Insets(5, 5, 5, 5);
         add(createPrefPanel(), c);
+        
+        c = new GridBagConstraints();
         c.gridx = 1;
+        c.gridy = 0;
+        c.fill = GridBagConstraints.BOTH;
+        c.weightx = 0.5;
+        c.weighty = 1.0;
         add(createDietPanel(), c);
-	}
-	
+    }
+    
     public JPanel createPrefPanel() {
         JPanel prefPanel = new JPanel();
-        prefPanel.setPreferredSize(new Dimension(300, 600));      
+        //prefPanel.setPreferredSize(new Dimension(300, 600));      
         prefPanel.setLayout(new BoxLayout(prefPanel, BoxLayout.PAGE_AXIS));
         prefPanel.add(createButtonGroup("Kostnad"));
         prefPanel.add(createButtonGroup("Svårighetsgrad"));
@@ -72,32 +85,19 @@ public class PreferencesPanel extends JPanel implements ActionListener {
     
     public JPanel createDietPanel() {
         JPanel dietPanel = new JPanel();
+        dietPanel.setLayout(new BoxLayout(dietPanel, BoxLayout.PAGE_AXIS));
+
         JLabel dietLabel = new JLabel("<html>När du väljer specialkost så " +
-        		"exkluderas recept som innehåller ingredienser från " +
-        		"ett fördefinierat antal kategorier från " +
-        		"receptförslagen.</html>");
+                "utesluts recept som innehåller ingredienser\nfrån " +
+                "ett fördefinierat antal kategorier från " +
+                "receptförslagen.</html>");
 
-        dietLabel.setVerticalAlignment(JLabel.CENTER);
-        dietLabel.setHorizontalAlignment(SwingConstants.LEFT);
-        dietLabel.setPreferredSize(new Dimension(300, 50));
+        dietLabel.setAlignmentX(Component.LEFT_ALIGNMENT);
+        dietPanel.add(dietLabel);
 
-        dietPanel.setPreferredSize(new Dimension(300, 600));
-        dietPanel.setLayout(new GridBagLayout());
-        GridBagConstraints c = new GridBagConstraints();
-        c.anchor 	= GridBagConstraints.FIRST_LINE_START;
-        c.fill		= GridBagConstraints.BOTH;
-        
-        c.gridx		= 0;
-        c.gridy		= 0;
-        c.weightx	= 0.5;
-        c.weighty	= 0;
-        dietPanel.add(dietLabel, c);
-
-        c.gridx		= 0;
-        c.gridy		= 1;
-        c.weightx	= 0.5;
-        c.weighty	= 0.5;
-        dietPanel.add(new DietPanel(), c);
+        DietPanel mainPanel = new DietPanel();
+        mainPanel.setAlignmentX(Component.LEFT_ALIGNMENT);
+        dietPanel.add(mainPanel);
 
         TitledBorder tb = BorderFactory.createTitledBorder(
                 BorderFactory.createLineBorder(Color.GRAY),
@@ -107,98 +107,108 @@ public class PreferencesPanel extends JPanel implements ActionListener {
                 new Font("Arial", Font.BOLD, 15));
         dietPanel.setBorder(tb);
 
+        Dimension preferredSize = dietPanel.getPreferredSize();
+        preferredSize.width = DIET_PANEL_PREFERRED_WIDTH;
+        dietPanel.setPreferredSize(preferredSize);
+
         return dietPanel;
     }    
 
     public String translate(String str) {
-    	if(str.equals("low"))
-    		return new String("låg");
-    	if(str.equals("medium"))
-    		return new String("mellan");
-    	if(str.equals("high"))
-    		return new String("hög");
-    	if(str.equals("easy"))
-    		return new String("lätt");
-    	if(str.equals("hard"))
-    		return new String("svår");
-    	if(str.equals("short"))
-    		return new String("kort");    	
-    	
-    	System.out.println("u: " + str);
-    	return new String("unknown word");
+        if(str.equals("low"))
+            return new String("låg");
+        if(str.equals("medium"))
+            return new String("mellan");
+        if(str.equals("high"))
+            return new String("hög");
+        if(str.equals("easy"))
+            return new String("lätt");
+        if(str.equals("hard"))
+            return new String("svår");
+        if(str.equals("short"))
+            return new String("kort");      
+        
+        System.out.println("u: " + str);
+        return new String("unknown word");
     }
     
-	public JPanel createButtonGroup(String title) {
-		// a column in a panel
-		JPanel radioPanel = new JPanel(new GridLayout(0,1));
-		ButtonGroup group = new ButtonGroup();
-		
-		// value is always in the range 1 to 3
-		for(int i=1; i<4; i++) {
-			JRadioButton button = new JRadioButton();
+    public JPanel createButtonGroup(String title) {
+        // a column in a panel
+        JPanel radioPanel = new JPanel();
+        radioPanel.setLayout(new BoxLayout(radioPanel, BoxLayout.LINE_AXIS));
 
-			// use mappings to get button labels from value
-			if(title.equals("Kostnad")) {
-				CostMap map = pv.getCostMap();
-				button.setText(translate(map.getValue(i)));
-				button.setActionCommand("cost" + i);
-			}
-			else if(title.equals("Svårighetsgrad")) {
-				DifficultyMap map = pv.getDifficultyMap();
-				button.setText(translate(map.getValue(i)));
-				button.setActionCommand("difficulty" + i);
-			}
-			else if(title.equals("Tillagningstid")) {
-				TimeMap map = pv.getTimeMap();
-				button.setText(translate(map.getValue(i)));
-				button.setActionCommand("time" + i);
-			}
-			group.add(button);
-			button.addActionListener(this);
-			radioPanel.add(button);
-		}
+        ButtonGroup group = new ButtonGroup();
+        
+        // value is always in the range 1 to 3
+        for(int i = 1; i < 4; i++) {
+            JRadioButton button = new JRadioButton();
+
+            // use mappings to get button labels from value
+            if(title.equals("Kostnad")) {
+                CostMap map = pv.getCostMap();
+                button.setText(translate(map.getValue(i)));
+                button.setActionCommand("cost" + i);
+            }
+            else if(title.equals("Svårighetsgrad")) {
+                DifficultyMap map = pv.getDifficultyMap();
+                button.setText(translate(map.getValue(i)));
+                button.setActionCommand("difficulty" + i);
+            }
+            else if(title.equals("Tillagningstid")) {
+                TimeMap map = pv.getTimeMap();
+                button.setText(translate(map.getValue(i)));
+                button.setActionCommand("time" + i);
+            }
+            group.add(button);
+            button.addActionListener(this);
+            radioPanel.add(button);
+        }
+
         TitledBorder tb = BorderFactory.createTitledBorder(
-        		BorderFactory.createEtchedBorder(),
-        		title,
-        		TitledBorder.CENTER,
-        		TitledBorder.CENTER,
-        		new Font("Arial", Font.BOLD, 13)
+                BorderFactory.createEtchedBorder(),
+                title,
+                TitledBorder.LEFT,
+                TitledBorder.TOP
+                //new Font("Arial", Font.BOLD, 13)
         );
+
         radioPanel.setBorder(tb);
+        radioPanel.setAlignmentX(Component.LEFT_ALIGNMENT);
+
         return radioPanel;
-	}
+    }
 
-	public void flashMsg(String msg) {
-		StatusPanel.getInstance().flash(msg, StatusPanel.INFO);
-	}
+    public void flashMsg(String msg) {
+        StatusPanel.getInstance().flash(msg, StatusPanel.INFO);
+    }
 
-	public void flashErrorMsg(String msg) {
-		StatusPanel.getInstance().flash(msg, StatusPanel.ERROR);
-	}	
-	
-	// update the user model with chosen value
-	public void actionPerformed(ActionEvent e) {
-		String s = e.getActionCommand();
-		if(s.startsWith("cost")) {
-			int desiredCost = Integer.parseInt(s.substring(s.length()-1));
-			user.setDesiredCost(desiredCost);
-			CostMap map = pv.getCostMap();
-			String value = translate(map.getValue(desiredCost));
-			flashMsg("Kostnad ändrad till " + value);
-		}
-		else if(s.startsWith("difficulty")){
-			int desiredDifficulty = Integer.parseInt(s.substring(s.length()-1));
-			user.setDesiredDifficulty(desiredDifficulty);
-			DifficultyMap map = pv.getDifficultyMap();
-			String value = translate(map.getValue(desiredDifficulty));
-			flashMsg("Svårighetsgrad ändrad till " + value);
-		}
-		else if(s.startsWith("time")){
-			int desiredTime = Integer.parseInt(s.substring(s.length()-1));
-			user.setDesiredTime(desiredTime);
-			TimeMap map = pv.getTimeMap();
-			String value = translate(map.getValue(desiredTime));
-			flashMsg("Tillagningstid ändrad till " + value);
-		}
-	}
+    public void flashErrorMsg(String msg) {
+        StatusPanel.getInstance().flash(msg, StatusPanel.ERROR);
+    }   
+    
+    // update the user model with chosen value
+    public void actionPerformed(ActionEvent e) {
+        String s = e.getActionCommand();
+        if(s.startsWith("cost")) {
+            int desiredCost = Integer.parseInt(s.substring(s.length()-1));
+            user.setDesiredCost(desiredCost);
+            CostMap map = pv.getCostMap();
+            String value = translate(map.getValue(desiredCost));
+            flashMsg("Kostnad ändrad till " + value);
+        }
+        else if(s.startsWith("difficulty")){
+            int desiredDifficulty = Integer.parseInt(s.substring(s.length()-1));
+            user.setDesiredDifficulty(desiredDifficulty);
+            DifficultyMap map = pv.getDifficultyMap();
+            String value = translate(map.getValue(desiredDifficulty));
+            flashMsg("Svårighetsgrad ändrad till " + value);
+        }
+        else if(s.startsWith("time")){
+            int desiredTime = Integer.parseInt(s.substring(s.length()-1));
+            user.setDesiredTime(desiredTime);
+            TimeMap map = pv.getTimeMap();
+            String value = translate(map.getValue(desiredTime));
+            flashMsg("Tillagningstid ändrad till " + value);
+        }
+    }
 }
